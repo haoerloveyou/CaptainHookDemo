75f7bec2c96cb41af3407724f5992501790f4f65
zerox333 <lawliet0914@gmail.com>
2013-05-01 18:09:32 +0800

    Initial Commit

---
+++ CaptainHookDemo/CaptainHookDemo.mm
@@ -0,0 +1,90 @@
+//
+//  CaptainHookDemo.mm
+//  CaptainHookDemo
+//
+//  Created by ZeroX on 13-5-1.
+//  Copyright (c) 2013年 __MyCompanyName__. All rights reserved.
+//
+
+// CaptainHook by Ryan Petrich
+// see https://github.com/rpetrich/CaptainHook/
+
+#import <Foundation/Foundation.h>
+#import "CaptainHook/CaptainHook.h"
+#include <notify.h> // not required; for examples only
+
+// Objective-C runtime hooking using CaptainHook:
+//   1. declare class using CHDeclareClass()
+//   2. load class using CHLoadClass() or CHLoadLateClass() in CHConstructor
+//   3. hook method using CHOptimizedMethod()
+//   4. register hook using CHHook() in CHConstructor
+//   5. (optionally) call old method using CHSuper()
+
+
+@interface CaptainHookDemo : NSObject
+
+@end
+
+@implementation CaptainHookDemo
+
+-(id)init
+{
+	if ((self = [super init]))
+	{
+	}
+
+    return self;
+}
+
+@end
+
+
+@class ClassToHook;
+
+CHDeclareClass(ClassToHook); // declare class
+
+CHOptimizedMethod(0, self, void, ClassToHook, messageName) // hook method (with no arguments and no return value)
+{
+	// write code here ...
+	
+	CHSuper(0, ClassToHook, messageName); // call old (original) method
+}
+
+CHOptimizedMethod(2, self, BOOL, ClassToHook, arg1, NSString*, value1, arg2, BOOL, value2) // hook method (with 2 arguments and a return value)
+{
+	// write code here ...
+
+	return CHSuper(2, ClassToHook, arg1, value1, arg2, value2); // call old (original) method and return its return value
+}
+
+static void WillEnterForeground(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef userInfo)
+{
+	// not required; for example only
+}
+
+static void ExternallyPostedNotification(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef userInfo)
+{
+	// not required; for example only
+}
+
+CHConstructor // code block that runs immediately upon load
+{
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+	
+	// listen for local notification (not required; for example only)
+	CFNotificationCenterRef center = CFNotificationCenterGetLocalCenter();
+	CFNotificationCenterAddObserver(center, NULL, WillEnterForeground, CFSTR("UIApplicationWillEnterForegroundNotification"), NULL, CFNotificationSuspensionBehaviorCoalesce);
+	
+	// listen for system-side notification (not required; for example only)
+	// this would be posted using: notify_post("com.zerox.CaptainHookDemo.eventname");
+	CFNotificationCenterRef darwin = CFNotificationCenterGetDarwinNotifyCenter();
+	CFNotificationCenterAddObserver(darwin, NULL, ExternallyPostedNotification, CFSTR("com.zerox.CaptainHookDemo.eventname"), NULL, CFNotificationSuspensionBehaviorCoalesce);
+	
+	// CHLoadClass(ClassToHook); // load class (that is "available now")
+	// CHLoadLateClass(ClassToHook);  // load class (that will be "available later")
+	
+	CHHook(0, ClassToHook, messageName); // register hook
+	CHHook(2, ClassToHook, arg1, arg2); // register hook
+	
+	[pool drain];
+}
